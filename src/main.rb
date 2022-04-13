278cbc67926db4208915138782a4a862e0ea8044
rikifujihara <rikifujihara@gmail.com>
2022-04-13 20:45:10 +1000

    Required JSON in main.rb and Added error handling to handle the error incase the file path for the JSON file is broken

--- src/main.rb
+++ src/main.rb
@@ -1,2 +1,9 @@
-file = File.read('./quiz_data.json')
-data_hash = JSON.parse(file)
+require 'json'
+
+begin
+    file = File.read('./quiz_data.json')
+    data_hash = JSON.parse(file)
+rescue
+    puts "I can't find the quiz data where it's supposed to be! Please check that the path for the JSON file or move the json file back to where it was if you've moved it. Thanks!"
+end
+
